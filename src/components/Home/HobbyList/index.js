8003486e678c77ab7963a86bf729a8aa2fe2b6d0
minhvu278 <dominhvu278@gmail.com>
2022-10-19 17:55:37 +0700

    first commit

---
+++ src/components/Home/HobbyList/index.js
@@ -0,0 +1,14 @@
+
+function HobbyList(props) {
+    const {hobbyList} = props;
+
+    return (
+        <ul>
+            {hobbyList.map(hobby => (
+                <li key={hobby.id}>{hobby.title}</li>
+            ))}
+        </ul>
+    )
+}
+
+export default HobbyList
